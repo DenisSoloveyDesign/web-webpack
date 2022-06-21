c405c6ecb5eb0fe5fabc5b06277cbc032079033e
NeutralWinter <neutralvvinter@gmail.com>
2022-06-21 15:01:50 +0300

    Update

--- app/js/index.js
+++ app/js/index.js
@@ -1,19 +1,20 @@
 //// ================================ Imports ======================================
 //scss
 import 'bootstrap/dist/css/bootstrap-grid.css';
+import 'nouislider/dist/nouislider.css';
 import '../scss/index.scss';
+
 //js
-import $ from 'jquery'; //Работает
-import Swiper from 'swiper'; //Работает
-import { Fancybox, Carousel, Panzoom } from '@fancyapps/ui'; //Работает
+import $ from 'jquery';
+import Swiper from 'swiper';
+import { Fancybox, Carousel, Panzoom } from '@fancyapps/ui';
 import IMask from 'imask';
 import Choices from 'choices.js';
 import autoComplete from '@tarekraafat/autocomplete.js';
 import validate from 'validate.js';
 import ApexCharts from 'apexcharts';
-import { scrolly } from 'scrolly';
-import tabs from './plugins/tabs';
 import { rippleEffect, Ripple } from 'data-ripple';
-import accordion from './plugins/accordion';
+import noUiSlider from 'nouislider';
+import Scrollbar from 'smooth-scrollbar';
 
 //// ================================ Code ======================================
